a1665dd493d12b16c32746654c8e77b0cbc538ae
John Paul <iamjohnpaulr5@gmail.com>
2024-06-11 10:32:32 +0530

    Refactor code to use ES6 module syntax

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
